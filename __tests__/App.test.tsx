f9ab4252c4307d5855925e17673d0e40061f748b
DangJingtao <dangjingtao@163.com>
2026-07-28 20:10:49 +0800

    test: isolate app smoke test from native navigation

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,14 +4,38 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: 'NavigationContainer',
+}));
+
+jest.mock('@react-navigation/native-stack', () => ({
+  createNativeStackNavigator: () => ({
+    Navigator: 'StackNavigator',
+    Screen: 'StackScreen',
+  }),
+}));
+
+jest.mock('../src/screens/SessionListScreen', () => ({
+  SessionListScreen: 'SessionListScreen',
+}));
+
+jest.mock('../src/screens/ChatScreen', () => ({
+  ChatScreen: 'ChatScreen',
+}));
+
+jest.mock('../src/screens/HostConfigScreen', () => ({
+  HostConfigScreen: 'HostConfigScreen',
+}));
+
 import App from '../App';
 
 describe('App', () => {
-  it('renders without crashing', async () => {
+  it('renders the application shell without crashing', async () => {
     let component: ReactTestRenderer.ReactTestRenderer | undefined;
     await ReactTestRenderer.act(async () => {
       component = ReactTestRenderer.create(<App />);
     });
-    expect(component).toBeDefined();
+    expect(component?.root.findByType('NavigationContainer')).toBeDefined();
   });
 });
