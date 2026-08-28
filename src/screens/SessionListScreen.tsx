54777656f01b80b6e45eda7fb938c71608a475fc
DangJingtao <dangjingtao@163.com>
2026-08-28 21:49:52 +0800

    fix: use supported absolute fill style for swipe actions

--- src/screens/SessionListScreen.tsx
+++ src/screens/SessionListScreen.tsx
@@ -649,7 +649,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   swipeActions: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     flexDirection: 'row',
     alignItems: 'stretch',
     justifyContent: 'flex-start',
